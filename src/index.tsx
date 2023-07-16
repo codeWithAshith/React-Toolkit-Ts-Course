ca867a0f3cd7fd6ab0f053f56c10bbfd370d860d
codewithashith <codewithashith@gmail.com>
2023-07-16 18:19:42 +0530

    01. Json-server Setup

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,11 @@ import "./index.css";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+// npm install -g json-server
+// To run
+// npm run serve-json
+
 root.render(
   <React.StrictMode>
     <p>Hello World</p>
